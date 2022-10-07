7b4d860a01ddb94b7bf21236f2942f397759dba8
krlooss3 <carloz.h321@gmail.com>
2022-10-06 19:23:38 -0600

    User Interface

---
+++ src/dao/models/entities/Users.ts
@@ -0,0 +1,8 @@
+export interface ICashFlow{
+    name: string;
+    lastName: string;
+    birth: Date;
+    gender: string;
+    direction: string;
+    _id?:unknown;
+};
---
+++ src/libs/Users/index.ts
@@ -0,0 +1,50 @@
+import {getConnection} from "@models/sqlite/SqliteConn";
+import { CashFlowDao } from "@models/sqlite/CashFlowDao";
+export interface ICashFlow{
+    type: 'INCOME' | 'EXPENSE';
+    date: Date;
+    amount: number;
+    description: string;
+    _id?:unknown;
+};
+
+export class CashFlow{
+    private dao: CashFlowDao;
+    public constructor(){
+        getConnection()
+            .then(conn=>{this.dao = new CashFlowDao (conn);})
+            .catch(ex=>console.error(ex));
+    }
+    private cashFlowItems: ICashFlow[]=[];
+
+    //Consultas
+   public getAllCashFlowItems(){
+        return this.dao.getCashFlows();
+    }
+
+    public getCashFlowByIndex(index: number){
+       return this.dao.getCashFlowById({_id: index});
+    }
+
+    public addCashFlow(cashFlow: ICashFlow) {
+        return this.dao.insertNewCashFlow(cashFlow);
+    }
+    public updateCashFlow(index: number, cashFlow:ICashFlow):boolean{
+        if(index>=0 && index < this.cashFlowItems.length){
+            this.cashFlowItems[index]= cashFlow;
+            return true;
+        }
+        return false;
+    }
+
+    public deleteCashFlow(index: number):boolean{
+        if(index>=0 && index < this.cashFlowItems.length){
+            this.cashFlowItems=this.cashFlowItems.filter(
+                (_obj:ICashFlow, i:number)=>i !==index
+            );
+            return true;
+        }
+        return false;
+    }
+}
+
